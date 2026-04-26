ce7c12fcb919b8545e7a5bc7d61834436a9c08da
Supaluck Singjan <supalucksingjan@gmail.com>
2026-04-26 22:20:06 +0700

    Swicth case sayWord package

--- src/pkg/sayWord.go
+++ src/pkg/sayWord.go
@@ -3,5 +3,25 @@ package sayword
 import "fmt" 
 
 func SayWord() {
-	fmt.Println("Hello Test")
+	var sayWord = "Monday"
+
+	switch sayWord {
+	case "Monday":
+		fmt.Println("วันจันทร์")
+	case "Tuesday":
+		fmt.Println("วันอังคาร")
+	case "Wednesday":
+		fmt.Println("วันพุธ")
+	case "Thursday":
+		fmt.Println("วันพฤหัส")
+	case "Friday":
+		fmt.Println("วันศุกร์")
+	case "Saturday":
+		fmt.Println("วันเสาร์")
+	case "Sanday":
+		fmt.Println("วันอาทิตย์")
+	default:
+		fmt.Println("ไม่มีคำพูดที่ตรงกัน")
+	}
+
 }
